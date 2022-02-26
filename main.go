659295e0c92831b3282245919eabda663306447b
Erhan Yakut <yakuter@gmail.com>
2022-02-26 23:04:00 +0300

    removed unnecessary filepath walker

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -24,97 +22,69 @@ func main() {
 		log.Fatal("Path argument is not defined")
 	}
 
-	rootPath := os.Args[1]
-
-	pathInfo, err := os.Lstat(rootPath)
-	if err != nil {
-		log.Fatalf("Failed to locate path:'%s' error: %v", rootPath, err)
-	}
-
-	if !pathInfo.IsDir() {
-		log.Fatalf("Path '%s' is not a directory", rootPath)
-	}
-
-	err = filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			log.Printf("Failed to walk dir path '%s' error: %v", path, err)
-			return nil
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		if filepath.Ext(path) != ".go" {
-			return nil
-		}
-
-		findGoStmts(path)
-
-		return nil
-
-	})
-
-	if err != nil {
-		log.Fatalf("filepath.Walkdir error: %v", err)
-	}
+	findGoStmts(os.Args[1])
 }
 
 func findGoStmts(path string) {
 	fset := token.NewFileSet()
-	astFile, err := parser.ParseFile(fset, path, nil, parser.AllErrors)
+	astPkgs, err := parser.ParseDir(fset, path, nil, parser.AllErrors)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse dir %s Error: %v", path, err)
 	}
 
-	// DEBUG
-	var v visitor
-	ast.Walk(v, astFile)
+	for _, pkg := range astPkgs {
+		for _, astFile := range pkg.Files {
 
-	ast.Inspect(astFile, func(n ast.Node) bool {
-		goStmt, ok := n.(*ast.GoStmt)
-		if !ok {
-			return true
-		}
+			// DEBUG. Uncomment to see the tree view
+			// var v visitor
+			// ast.Walk(v, astFile)
 
-		fmt.Printf("Go statement found at file: %s position: %d \n", path, goStmt.Pos())
-
-		call := goStmt.Call
-		var err error
-
-		switch fun := call.Fun.(type) {
-		case *ast.FuncLit:
-			if !checkFuncLit(fun) {
-				err = fmt.Errorf("first statement should be '%s'", panicDefer)
-			}
-		case *ast.Ident:
-			if fun.Name != panicFn {
-				err = fmt.Errorf("deferred function should be '%s()'", panicFn)
-			}
-		case *ast.SelectorExpr:
-			pkg, ok := fun.X.(*ast.Ident)
-			if ok {
-				if pkg.Name != panicPkg {
-					err = fmt.Errorf("deferred function should call '%s()' in '%s' package", panicFn, panicPkg)
+			ast.Inspect(astFile, func(n ast.Node) bool {
+				goStmt, ok := n.(*ast.GoStmt)
+				if !ok {
+					return true
 				}
-			}
 
-			if fun.Sel.Name != panicFn {
-				err = fmt.Errorf("deferred function should be '%s()'", panicFn)
-			}
-		default:
-			err = fmt.Errorf("go statement should always call a func lit")
+				fmt.Printf("Go statement found at file: %s position: %d \n", path, goStmt.Pos())
+
+				call := goStmt.Call
+				var err error
+
+				switch fun := call.Fun.(type) {
+				case *ast.FuncLit:
+					if !checkFuncLit(fun) {
+						err = fmt.Errorf("first statement should be '%s'", panicDefer)
+					}
+				case *ast.Ident:
+					if fun.Name != panicFn {
+						err = fmt.Errorf("deferred function should be '%s()'", panicFn)
+					}
+				case *ast.SelectorExpr:
+					pkg, ok := fun.X.(*ast.Ident)
+					if ok {
+						if pkg.Name != panicPkg {
+							err = fmt.Errorf("deferred function should call '%s()' in '%s' package", panicFn, panicPkg)
+						}
+					}
+
+					if fun.Sel.Name != panicFn {
+						err = fmt.Errorf("deferred function should be '%s()'", panicFn)
+					}
+				default:
+					err = fmt.Errorf("go statement should always call a func lit")
+				}
+
+				if err != nil {
+					fmt.Printf("Error: %v\n\n", err)
+					return false
+				}
+
+				fmt.Println("Successful statement :)")
+
+				return true
+			})
 		}
-
-		if err != nil {
-			fmt.Printf("Error: %v\n\n", err)
-			return false
-		}
-
-		fmt.Println("Successful statement :)")
-
-		return true
-	})
+	}
 }
 
 func checkFuncLit(fl *ast.FuncLit) bool {
